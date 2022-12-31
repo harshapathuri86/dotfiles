621a582a507936af733b57015e3f0c822e127411
Harsha Pathuri <harsha.pathuri@students.iiit.ac.in>
2022-12-31 14:28:11 +0530

    update: added all known files

---
+++ config/fish/config.fish
@@ -0,0 +1,33 @@
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.emacs.d/bin"
+export PATH="$PATH:$HOME/.local/share/gem/ruby/3.0.0/bin"
+# source /opt/intel/oneapi/setvars.sh
+
+export PATH="$PATH:$HOME/.cargo/bin"
+#export PATH="$PATH:$HOME/go/bin" # added to fish using 
+# set -Ux GOPATH $HOME/go
+# fish_add_path $GOPATH/bin
+export PATH="$PATH:$HOME/.local/bin/statusbar"
+
+alias mkd="mkdir -pv"
+alias yt="yt-dlp --embed-metadata -i"
+alias yta="yt -x -f bestaudio/best"
+alias ffmpeg="ffmpeg -hide_banner"
+
+alias ls="ls -hN --color=auto --group-directories-first"
+alias grep="grep --color=auto"
+alias diff="diff --color=auto"
+alias ip="ip -color=auto"
+
+alias em='emacsclient --tty'
+#alias ema='emacsclient --create-frame --no-wait'
+#alias lf='lfub'
+alias g="git"
+alias p="pacman"
+
+pyenv init - | source
+zoxide init fish | source
+
+export EDITOR="nvim"
+export TERMINAL="alacritty"
+export BROWSER="firefox"
