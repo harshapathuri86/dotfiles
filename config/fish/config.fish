0b8d7ab36b6e2e3f9f49f907cae88a992f2bcfff
Pathuri Venkata Sri Harsha <sri.harsha@Cfadmins-MacBook-Pro.local>
2025-01-25 13:46:22 +0530

    added ghostty skhd :: updated fish :: removed doom emacs and yabai

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,49 +1,28 @@
-export PATH="$PATH:$HOME/.local/bin"
-export PATH="$PATH:$HOME/.local/share/gem/ruby/3.0.0/bin"
-# source /opt/intel/oneapi/setvars.sh
-
-export PATH="$PATH:$HOME/.cargo/bin"
-#export PATH="$PATH:$HOME/go/bin" # added to fish using
-# set -Ux GOPATH $HOME/go
-# fish_add_path $GOPATH/bin
-export PATH="$PATH:$HOME/.local/bin/statusbar"
-
-alias mkd="mkdir -pv"
-alias yt="yt-dlp --embed-metadata -i"
-alias yta="yt -x -f bestaudio/best"
-alias ffmpeg="ffmpeg -hide_banner"
-
-alias ls="ls -hN --color=auto --group-directories-first"
-alias grep="grep --color=auto"
-alias diff="diff --color=auto"
-alias ip="ip -color=auto"
-
-alias g="git"
-alias p="pacman"
-
-alias rpall="sudo pacman -R $(pacman -Qtdq)"
-# sudo pacman -R $(pacman -Qqs package_name)
-
-# pyenv init - | source
 zoxide init fish | source
 
-export EDITOR="nvim"
-export TERMINAL="alacritty"
-export BROWSER="firefox"
+set EDITOR "$(which nvim)"
 
-# Start X at login
-# https://wiki.archlinux.org/title/fish
-# if status is-login
-#     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-#         exec startx -- -keeptty
-#     end
-# end
+source $HOME/.venv/bin/activate.fish
 
-# status --is-interactive; and pyenv virtualenv-init - | source
-# export MPD_HOST=$HOME/.config/mpd/socket
+alias v 'nvim'
+alias vl 'nvim leetcode.nvim'
+alias g 'git'
+alias gc 'git clone'
+alias cc 'git commit -m'
+alias gs 'git status'
+alias ga 'git add'
+alias gp 'git push'
+alias gd 'git diff'
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/harsha/.ghcup/bin # ghcup-env
+# brew
+alias bi 'brew install'
+alias bu 'brew update'
+alias bs 'brew search'
+alias bd 'brew doctor'
+alias bl 'brew list'
+alias br 'brew reinstall'
 
-# if test "$TERM" = "xterm-kitty"
-#     alias ssh "kitty +kitten ssh"
-# end
+alias jumpcloud "echo passwd | totp-cli g --show-remaining cashfree jumpcloud | cut -d ' ' -f 1 | tr -d '\n' | pbcopy"
+alias vpn "echo passwd | totp-cli g --show-remaining cashfree vpn | cut -d ' ' -f 1 | tr -d '\n' | pbcopy"
+alias cashfree "echo passwd | totp-cli g --show-remaining cashfree $argv[1] | cut -d ' ' -f 1 | tr -d '\n' | pbcopy"
+alias k "kubectl --insecure-skip-tls-verify "
